e343fb12a3068415e38c5eb586fe6f66f0df9877
marcotristani <marco.tristani@hotmail.it>
2026-04-08 22:44:22 +0200

    primo esercizio input e formattazione testo generato

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,8 +1,33 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        System.out.println("prova");
+        String nome;
+        String cognome;
+        String colorePreferito;
+        int giornoNascita;
+        int meseNascita;
+        int annoNascita;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Inserire Nome:");
+        nome = input.nextLine();
+        System.out.println("Inserire Cognome:");
+        cognome = input.nextLine();
+        System.out.println("Inserire Colore preferito:");
+        colorePreferito = input.nextLine();
+        System.out.println("Inserire giorno di nascita:");
+        giornoNascita = input.nextInt();
+        System.out.println("Inserire mese di nascita:");
+        meseNascita = input.nextInt();
+        System.out.println("Inserire anno di nascita:");
+        annoNascita = input.nextInt();
+        int sumDate = giornoNascita + meseNascita + annoNascita;
+        input.close();
+        System.out.printf("La password generata è : %s-%s-%s-%d", nome, cognome, colorePreferito, sumDate);
     }
 
 }
